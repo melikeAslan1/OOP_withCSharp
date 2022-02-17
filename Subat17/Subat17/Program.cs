4bb3e1857fc9853b6d7fdf038af8f0138cb49740
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-02-17 14:56:41 +0300

    subat17

---
+++ Subat17/Subat17/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subat17
+{
+    class Araba
+    {
+        public virtual void Sur()
+        {
+            Console.WriteLine("Araba Sur...");
+        }
+    }
+
+    class Ferrari: Araba
+    {
+        public override void Sur()
+        {
+            Console.WriteLine("Ferrari Sur...");
+        }
+
+    }
+
+    class Mercedes:Araba
+    {
+        public override void Sur()
+        {
+            Console.WriteLine("Mercedes Sur...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //polimorfizm:
+
+            //Araba araba = new Araba();
+            //Ferrari ferrari = new Ferrari();
+            //Mercedes mercedes = new Mercedes();
+
+            Araba araba = new Araba();
+            Araba ferrari = new Ferrari();
+            Araba mercedes = new Mercedes();
+            araba.Sur();
+            ferrari.Sur();
+            mercedes.Sur();
+
+            Object obj = new Ferrari();
+            ((Ferrari)obj).Sur();
+
+
+
+        }
+    }
+}
